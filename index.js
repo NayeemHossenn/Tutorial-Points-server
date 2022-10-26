361d1cfd29d6708cad80dc17cfdc47b60e38ba7c
NayeemHossenn <nayeemhossen245@gmail.com.com>
2022-10-26 13:55:18 +0600

    courses data added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.port || 5000;
 
+app.use(cors());
+
 const categories = require("./data/category.json");
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Tutorial api is running");
@@ -10,6 +14,21 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const courseCategory = courses.filter((course) => course.category_id === id);
+  res.send(courseCategory);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const AllCourses = courses.find((course) => course._id === id);
+  res.send(AllCourses);
 });
 
 app.listen(port, () => {
