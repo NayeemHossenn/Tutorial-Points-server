2ed6ba9b3875dc3eca4ada65c36e6683b47db616
NayeemHossenn <nayeemhossen245@gmail.com.com>
2022-10-26 17:08:45 +0600

    remove category data

--- index.js
+++ index.js
@@ -5,24 +5,14 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 
-const categories = require("./data/category.json");
 const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Tutorial api is running");
 });
 
-app.get("/categories", (req, res) => {
-  res.send(categories);
-});
 app.get("/courses", (req, res) => {
   res.send(courses);
-});
-
-app.get("/category/:id", (req, res) => {
-  const id = req.params.id;
-  const courseCategory = courses.filter((course) => course.category_id === id);
-  res.send(courseCategory);
 });
 
 app.get("/courses/:id", (req, res) => {
